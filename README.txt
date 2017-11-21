4df685ce599ce18d8643ed1310e5d9a1f12d7050
Anthony Tracy <gotbordom@gmail.com>
2017-11-21 15:27:48 -0700

    made small updates..

--- README.txt
+++ README.txt
@@ -5,3 +5,14 @@ Description:
 This is primaraly for doing signal analysis with one of my projects that I am bored and doing.
 
 Not likely going to be usefultoanyone but me...
+
+What is in this repo?
+
+loadSpectra.py: 
+  - If you run this file with an HDF5 file with the spectra it will load all the datasets into python np.arrays
+    - 1st argument is hdf5 path/filename
+    - 2nd argument is dataset you want loaded from the hdf5 file
+		- May want HDFView software if youo don't known the structure of your hdf5 file. 
+		- Without the second argument this will just load all datasets it finds... lots of data...
+
+
--- loadSpectra.py
+++ loadSpectra.py
@@ -2,11 +2,30 @@
 
 import numpy as np
 import h5py as h5
+import argparse
 
 # This file is just for loading my HDF5 files and making them python readable...
 
-fName = 'ThisIsATest.hdf5'
+ap = argparse.ArgumentParser()
+ap.add_argument("-fN",
+				required=True,
+				type = str,
+				help = "Path/filename of hdf5 file")
+ap.add_argument("-ds",
+				required=False,
+				default='NA',
+				type = str,
+				help = "Name of specific dataset to load")
+args = vars(ap.parse_args())
+
+# Some helper functinos:
+def printname(name):
+# This is really just for looking at every directory in the hdf5 file...
+  print name
+
+# Get filename && dataset
+fName = args(['fN'])
+dsName = args(['ds'])
 
 dF = h5.File(fName,'r')
-
 
